a9225ddefcb7708d5ecc456ee7636c8fed76c565
Blaine Howerton <bhowerton@yghdtv.com>
2022-06-07 16:32:55 -0600

    updated article controller to create section

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,11 +15,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
     @editions = Edition.all
+    @sections = Section.all
   end
 
   def create
     @article = Article.new(article_params)
     @article.print_date = params[:print_date]
+    @article.section_id = params[:section_id]
 
     if @article.save
       redirect_to @article
