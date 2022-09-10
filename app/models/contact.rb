bf9b886944f55f95fb935b7dbf26ca70c9268e7b
Blaine Howerton <bhowerton@yghdtv.com>
2022-09-10 12:26:48 -0600

    model error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,10 @@ class Contact < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+belongs_to :organization
+
+validates :first, presence: true
+validates :last, presence: true
 
 end
 
